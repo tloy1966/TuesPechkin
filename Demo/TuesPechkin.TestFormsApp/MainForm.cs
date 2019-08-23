2955ffbe286da9ccc156fbc683da09c796ebe8ff
mchien <max.chien@oath.com>
2019-08-23 20:48:11 +0800

    Provide PDFHelper to avoid get converter in wrong way
    - Works in x86 or x64 environment
    - Works in IIS based or winform application

---
+++ Demo/TuesPechkin.TestFormsApp/MainForm.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Windows.Forms;
+using TuesPechkin;
+using TuesPechkin.Wkhtmltox.AnyCPU;
+
+namespace Html2PdfTestApp
+{
+	public partial class MainForm : Form
+	{
+		private IConverter converter = PDFHelper.Factory.GetConverter();
+
+		private HtmlToPdfDocument Document = new HtmlToPdfDocument
+		{
+			Objects =
+			{
+				new ObjectSettings { PageUrl = "www.google.com" }
+			}
+		};
+
+		public MainForm()
+		{
+			this.InitializeComponent();
+
+			this.globalSettingsPropertyGrid.SelectedObject = this.Document;
+		}
+
+		private void OnConvertButtonClick(object sender, EventArgs e)
+		{
+			byte[] buf = null;
+
+			try
+			{
+				buf = converter.Convert(this.Document);
+				MessageBox.Show("All conversions done");
+
+				if (buf == null)
+				{
+					MessageBox.Show(
+						"No exceptions were raised but wkhtmltopdf failed to convert.",
+						"Error Converting");
+
+					return;
+				}
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.Message, "Exception Occurred");
+			}
+
+			try
+			{
+				string fn = string.Format("{0}.pdf", Path.GetTempFileName());
+
+				FileStream fs = new FileStream(fn, FileMode.Create);
+				fs.Write(buf, 0, buf.Length);
+				fs.Close();
+
+				Process myProcess = new Process();
+				myProcess.StartInfo.FileName = fn;
+				myProcess.Start();
+			}
+			catch
+			{
+			}
+		}
+
+		private void OnLoad(object sender, EventArgs e)
+		{
+		}
+	}
+}
---
+++ Source/TuesPechkin.Wkhtmltox.AnyCPU/PDFHelper.cs
@@ -0,0 +1,51 @@
+﻿using System.Web.Hosting;
+
+namespace TuesPechkin.Wkhtmltox.AnyCPU
+{
+	public class PDFHelper
+	{
+		public class Factory
+		{
+			/// <summary>
+			/// The static readonly locker
+			/// </summary>
+			private static readonly object Locker = new object();
+
+			/// <summary>
+			/// Pdf converter
+			/// </summary>
+			private static IConverter converter;
+
+			/// <summary>
+			/// Singleton converter, for multi-threaded application
+			/// </summary>
+			/// <returns>Pdf converter</returns>
+			public static IConverter GetConverter()
+			{
+				lock (Locker)
+				{
+					if (converter != null)
+					{
+						return converter;
+					}
+
+					var tempFolderDeployment = new TempFolderDeployment();
+					var winAnyCpuEmbeddedDeployment = new WinAnyCPUEmbeddedDeployment(tempFolderDeployment);
+					IToolset toolSet;
+					if (HostingEnvironment.IsHosted)
+					{
+						toolSet = new RemotingToolset<PdfToolset>(winAnyCpuEmbeddedDeployment);
+					}
+					else
+					{
+						toolSet = new PdfToolset(winAnyCpuEmbeddedDeployment);
+					}
+
+					converter = new ThreadSafeConverter(toolSet);
+				}
+
+				return converter;
+			}
+		}
+	}
+}
